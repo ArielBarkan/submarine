df49b81ecb24ede331653d1938af34663a74556f
Ariel Barkan <ariel@peekapost.com>
2017-09-10 20:49:16 +0300

    First

--- battleship.php
+++ battleship.php
@@ -11,7 +11,7 @@ ini_set('display_errors', 1);
 
 
 
-init(4, "A2 B3,B4 D4", "D1 A2 C3");
+init(4, "A2 B3,B4 D4", "D1 A2 C3"); // OUTPUT 1,0 (1 hits, 0 sunk)
 init(10, "B2 E4,G6 G9,H3 J3", "D1 A2 B4 C4 B2 I3"); // OUTPUT 4,0 (4 hits, 0 sunk)
 init(5, "B2 B3,C4 E5", "B2 B3 D5 D1"); // OUTPUT 3,1 (3 hits, 1 sunk)
 
@@ -19,10 +19,10 @@ init(5, "B2 B3,C4 E5", "B2 B3 D5 D1"); // OUTPUT 3,1 (3 hits, 1 sunk)
 
 function init($N, $S, $T){
 
-/*    echo "<b>Start and End point of ships (from top left to  bottom right)</b>  = ".$S."<br>";
+    echo "<b>Start and End point of ships (from top left to  bottom right)</b>  = ".$S."<br>";
     echo "<b>Missiles fired by enemy</b>= ".$T."<br>";
     echo "<b>Game board size (provided but not relevant to the solution)</b> = $N X $N <br>";
-    echo "Solution (Number of hits on ships, number of sunken ships)<br>";*/
+    echo "Solution (Number of hits on ships, number of sunken ships)<br>";
 
     unset($N);
     echo getShips($S, $T);
